07f1a316605e7c91787924cca64989a1ed45dd05
NhutRoblox <jaryl20110@gmail.com>
2026-06-25 21:45:22 +0700

    Update taplamscript.lua

--- taplamscript.lua
+++ taplamscript.lua
@@ -1,33 +1,35 @@
 local TweenService = game:GetService("TweenService")
 
 local gui = script.Parent
-local openButton = gui.OpenButton
-local mainFrame = gui.MainFrame
-local closeButton = mainFrame.CloseButton
 
--- Vị trí đóng và mở
-local closedPos = UDim2.new(-0.35, 0, 0.25, 0)
-local openedPos = UDim2.new(0.05, 0, 0.25, 0)
+local openButton = gui:WaitForChild("OpenButton")
+local mainFrame = gui:WaitForChild("MainFrame")
+local closeButton = mainFrame:WaitForChild("CloseButton")
+
+local closedPos = UDim2.new(-0.35,0,0.25,0)
+local openedPos = UDim2.new(0.05,0,0.25,0)
 
 mainFrame.Position = closedPos
 mainFrame.Visible = true
 
 local tweenInfo = TweenInfo.new(
-    0.4,
-    Enum.EasingStyle.Quad,
-    Enum.EasingDirection.Out
+	0.4,
+	Enum.EasingStyle.Quad,
+	Enum.EasingDirection.Out
 )
 
 local function openMenu()
-    TweenService:Create(mainFrame, tweenInfo, {
-        Position = openedPos
-    }):Play()
+	print("Mở menu")
+	TweenService:Create(mainFrame,tweenInfo,{
+		Position = openedPos
+	}):Play()
 end
 
 local function closeMenu()
-    TweenService:Create(mainFrame, tweenInfo, {
-        Position = closedPos
-    }):Play()
+	print("Đóng menu")
+	TweenService:Create(mainFrame,tweenInfo,{
+		Position = closedPos
+	}):Play()
 end
 
 openButton.MouseButton1Click:Connect(openMenu)
